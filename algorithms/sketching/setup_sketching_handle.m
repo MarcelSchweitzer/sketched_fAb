ed58c28a7dfd80709c99a2ec046d96187b4afd26
Marcel Schweitzer <74645256+MarcelSchweitzer@users.noreply.github.com>
2022-09-21 09:41:36 +0200

    fixed small typo in setup_sketching_handle.m
    The vector resulting from subsampled DCT needs to be divided by sqrt(s/N) instead of sqrt(s*N). Doesn't make a difference for sketched FOM, though

--- algorithms/sketching/setup_sketching_handle.m
+++ algorithms/sketching/setup_sketching_handle.m
@@ -2,4 +2,4 @@ function hS = setup_sketching_handle(N,s)
 
 E = spdiags(2*round(rand(N,1))-1,0,N,N); % Rademacher
 D = speye(N); D = D(randperm(N,s),:);
-hS = @(X) D*dct(E*X)/sqrt(N*s);
+hS = @(X) D*dct(E*X)/sqrt(s/N);
